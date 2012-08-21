e27a7b9f0509fa754d7d94ac3df039ae86f8705d
chinesejie <chinesejie@qq.com>
2012-08-21 17:14:02 +0800

    chinesejie

---
+++ sqlserver-odbc/src/main/java/com/microsoft/sqlserver/sqlserver_odbc/App.java
@@ -0,0 +1,13 @@
+package com.microsoft.sqlserver.sqlserver_odbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
