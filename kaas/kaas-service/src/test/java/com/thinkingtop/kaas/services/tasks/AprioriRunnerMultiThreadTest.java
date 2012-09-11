9601a49f8ea51f234d8f2752b351acd09a38bc76
Deng YuFu <954068039@qq.com>
2012-09-11 22:51:36 +0800

    add AprioriRunnerMultiThread,but not completed

---
+++ kaas/kaas-service/src/test/java/com/thinkingtop/kaas/services/tasks/AprioriRunnerMultiThreadTest.java
@@ -0,0 +1,19 @@
+package com.thinkingtop.kaas.services.tasks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AprioriRunnerMultiThreadTest {
+
+	@Test
+	public void test() {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+		AprioriRunnerMultiThread service = (AprioriRunnerMultiThread)ctx.getBean("aprioriRunnerMultiThread");
+		System.out.println(service.getClass());
+		service.runIt();
+		ctx.destroy();
+	}
+
+}
---
+++ kaas/kaas-service/src/test/java/com/thinkingtop/kaas/services/tasks/myThreadPoolTaskExecutorTest.java
@@ -0,0 +1,19 @@
+package com.thinkingtop.kaas.services.tasks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class myThreadPoolTaskExecutorTest {
+
+	@Test
+	public void testRunIt() {
+		/*ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+		myThreadPoolTaskExecutor service = (myThreadPoolTaskExecutor)ctx.getBean("myThreadPoolTaskExecutor");
+		System.out.println(service.getClass());
+		service.runIt();
+		ctx.destroy();*/
+	}
+
+}
