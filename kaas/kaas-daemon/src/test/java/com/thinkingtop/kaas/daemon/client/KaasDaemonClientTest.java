1649f31a7062b79fdaf4cd458828ee753ebc8c76
chinesejie <chinesejie@qq.com>
2012-09-18 14:48:11 +0800

    add test server

---
+++ kaas/kaas-daemon/src/test/java/com/thinkingtop/kaas/daemon/client/KaasDaemonClientTest.java
@@ -0,0 +1,66 @@
+package com.thinkingtop.kaas.daemon.client;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.mortbay.http.HttpContext;
+import org.mortbay.http.HttpServer;
+import org.mortbay.http.SocketListener;
+import org.mortbay.http.handler.ResourceHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Unit test for simple App.
+ */
+public class KaasDaemonClientTest {
+	private static final Logger logger = LoggerFactory.getLogger(KaasDaemonClientTest.class.getName());
+
+	private static HttpServer server;
+
+	@BeforeClass
+	public static void beforeClass() {
+		// creata Jetty HttpServer
+		server = new HttpServer();
+		// set listener on port 8080，receive HTTP request 
+		SocketListener listener = new SocketListener();
+		listener.setPort(8080);
+		server.addListener(listener);
+
+		// create HttpContext，deal with HTTPrequest
+		HttpContext context = new HttpContext();
+		// setContextPath set root URL
+		context.setContextPath("/kaas");
+		// setResourceBase set resource base
+		context.setResourceBase("../dist");
+
+		context.addHandler(new ResourceHandler());
+		server.addContext(context);
+		// start jetty
+		try {
+			server.start();
+			logger.info("jetty start");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		try {
+			server.stop();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testkaasDaemonClient() {
+		logger.info("1");
+		logger.info("1");
+		logger.info("1");
+		logger.info("1");
+	}
+
+}
