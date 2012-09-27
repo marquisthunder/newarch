305d4bf1d1ad0cef27e397757ec32f50a4d19e31
DengYuFu <954068039@qq.com>
2012-09-27 17:08:19 +0800

    Automatic detection

--- kaas/kaas-service/src/main/java/com/thinkingtop/kaas/services/manage/AlgorithmManage.java
+++ kaas/kaas-service/src/main/java/com/thinkingtop/kaas/services/manage/AlgorithmManage.java
@@ -3,29 +3,55 @@ package com.thinkingtop.kaas.services.manage;
 import java.util.Map;
 import java.util.Set;
 
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import com.thinkingtop.kaas.services.algorithm.Algorithm;
 import com.thinkingtop.kaas.services.algorithm.impl.AlgorithmDefault;
 import com.thinkingtop.kaas.services.util.ClassUtil;
 
+@Component("algorithmManage")
 public class AlgorithmManage {
+	static Logger logger=Logger.getLogger(AlgorithmManage.class);
 	private Algorithm myAlgorithm;
 	private Map<String,Algorithm> algorithms;
 	
 	public Map<String, Algorithm> getAlgorithms() {
 		return algorithms;
 	}
+	
+	@Resource()
 	public void setAlgorithms(Map<String, Algorithm> algorithms) {
+		for(Map.Entry a : algorithms.entrySet()){
+			logger.info("algorithm:-- "+a);
+		}
 		this.algorithms = algorithms;
+		/*Set<Class<?>> classstr = ClassUtil.getClasses(AlgorithmDefault.class.getPackage());
+		for(Class<?> c : classstr){
+			String algorithmClassName = c.getName();
+			if(algorithmClassName.matches(".+\\.[\\w]+$")){
+				try {
+					int i = algorithmClassName.lastIndexOf(".");
+					String name = algorithmClassName.substring(i+1);
+					Algorithm a = (Algorithm)c.newInstance();
+					this.algorithms.put(name, a);
+				} catch (InstantiationException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			}
+		}*/
 	}
 	public Algorithm getMyAlgorithm() {
 		return myAlgorithm;
 	}
 	
+	@Resource(name="algorithmDefault")
 	public void setMyAlgorithm(Algorithm myAlgorithm) {
 		this.myAlgorithm = myAlgorithm;
-		
 	}
 	
 	public String process(String algorithmName){
@@ -39,6 +65,7 @@ public class AlgorithmManage {
 	}
 	
 	public void runIt() {
+		logger.info("myAlgorithm:----"+myAlgorithm);
 		myAlgorithm.runIt();
 	}
 	public String[] getRecommend(String inputItems, int outputItemsNum,
--- kaas/kaas-service/src/test/java/com/thinkingtop/kaas/services/manage/AlgorithmManageTest.java
+++ kaas/kaas-service/src/test/java/com/thinkingtop/kaas/services/manage/AlgorithmManageTest.java
@@ -2,6 +2,9 @@ package com.thinkingtop.kaas.services.manage;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,18 +15,21 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.thinkingtop.kaas.service.util.BeforeTest;
+import com.thinkingtop.kaas.services.algorithm.Algorithm;
+import com.thinkingtop.kaas.services.algorithm.impl.AprioriRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:beans.xml")
 public class AlgorithmManageTest {
-
+	static Logger logger=Logger.getLogger(AlgorithmManageTest.class);
+	
 	@Qualifier("algorithmManage")
 	@Autowired
 	private AlgorithmManage algorithmManage;
 	
 	@Test
 	public void testProcess() {
-		String s = algorithmManage.process("apriori");
+		String s = algorithmManage.process("aprioriRunner");
 		Assert.assertEquals("yes",s);
 		s = algorithmManage.process("aprioriaaa");
 		Assert.assertEquals(false,s.equals("yes"));
@@ -31,9 +37,18 @@ public class AlgorithmManageTest {
 	
 	@Test
 	public void testGetRecommend() {
-		algorithmManage.process("apriori");
+		String s = algorithmManage.process("aprioriRunner");
 		algorithmManage.runIt();
 		String[] a = algorithmManage.getRecommend("1,2", 2, 1);
+		//logger.info("algorithmManage myAlgorithm:"+a[0]);
 		Assert.assertEquals("3,6",a[0]);
+	}
+	
+	@Test
+	public void testGetAlgorithms() {
+		Map<String,Algorithm> algorithms = algorithmManage.getAlgorithms();
+		for(Map.Entry a : algorithms.entrySet()){
+			logger.info("algorithm:-- "+a);
+		}
 	}
 }
