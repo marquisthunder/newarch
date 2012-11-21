9801a9f2b5cee4b42b5445a2293fe9bb502d347f
DengYuFu <954068039@qq.com>
2012-11-21 17:02:39 +0800

    update

---
+++ kaas/kaas-ecWeb/kaas-ecWebSSH/src/test/java/com/thinkingtop/kaasservice/KaasServiceManageTest.java
@@ -0,0 +1,27 @@
+package com.thinkingtop.kaasservice;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:beans.xml")
+public class KaasServiceManageTest {
+	static Logger logger=Logger.getLogger(KaasServiceManageTest.class);
+	
+	@Autowired
+	private KaasServiceManage kaasServiceManage;
+	@Test
+	public void test() {
+		System.out.println("----------------");
+		kaasServiceManage.getExclusiveKeyService();
+		//KaasService kaasService = new KaasService();
+		/*ExclusiveKeyService eks = kaasService.getExclusiveKeyServicePort();
+		List<String> a = eks.test("afdsa");
+		logger.info("test----------:"+eks.test("afdsa"));*/
+	}
+}
